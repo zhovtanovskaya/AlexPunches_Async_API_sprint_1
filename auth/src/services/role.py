194fa02c99d8e27b14da5a3549794edad5003e03
Y.Zhovtanovskaya <y.zhovtanovskaya@gmail.com>
2022-11-03 17:21:25 +0600

    Добавлена документация в services.role #12

--- auth/src/services/role.py
+++ auth/src/services/role.py
@@ -1,3 +1,5 @@
+"""Сервис управления ролями в БД."""
+
 from flask_sqlalchemy.extension import SQLAlchemy
 from sqlalchemy.exc import IntegrityError
 
@@ -6,15 +8,22 @@ import services.models.role as services_models
 
 
 class RoleService:
-    """Сервис для управления ролями."""
+    """Сервис для доступа и редактирования ролей в БД."""
 
     def __init__(self, rdb: SQLAlchemy):
+        """Проинициализировать подключение к БД."""
         self.rdb = rdb
 
     def create_role(
             self,
-            role: services_models.Role
-            ) -> services_models.Role:
+            role: services_models.Role,
+            ) -> services_models.Role | None:
+        """Создать роль в базе данных.
+
+        Returns:
+            Новый объект, представляющий запись в БД.  Или None
+            в случае ошибки.
+        """
         orm_role = orm_models.Role(
             name=role.name,
             description=role.description,
@@ -24,20 +33,32 @@ class RoleService:
             self.rdb.session.commit()
         except IntegrityError:
             self.rdb.session.rollback()
+            return None
         return services_models.Role.from_orm(orm_role)
 
     @staticmethod
     def get_roles_list() -> list[services_models.Role]:
+        """Получить список всех ролей в БД."""
         roles = orm_models.Role.query.all()
         return [services_models.Role.from_orm(r) for r in roles]
 
     def delete_role(self, role_id: int):
+        """Удалить роль из базы данных, если существует."""
         orm_role = orm_models.Role.query.get(role_id)
         if orm_role:
             self.rdb.session.delete(orm_role)
             self.rdb.session.commit()
 
-    def update_role(self, role: services_models.Role) -> services_models.Role:
+    def update_role(
+            self,
+            role: services_models.Role,
+            ) -> services_models.Role | None:
+        """Обновить роль в базе данных.
+
+        Returns:
+            Новый объект, представляющий обновленную запись в БД.
+            Или None, если записи с role.id в БД не существует.
+        """
         if role.id is None:
             return
         orm_role = orm_models.Role.query.get(role.id)
@@ -45,4 +66,5 @@ class RoleService:
             for field, value in role.dict().items():
                 setattr(orm_role, field, value)
             self.rdb.session.add(orm_role)
+            self.rdb.session.commit()
             return services_models.Role.from_orm(orm_role)
