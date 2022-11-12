8f21a498d7c17bd07e00b6075fcecacca2d630c1
Punches <punche@yandex.ru>
2022-11-13 01:30:08 +0300

    добавить алембик в тесты, вместо DDL

--- auth/tests/functional/fixtures/data.py
+++ auth/tests/functional/fixtures/data.py
@@ -6,7 +6,6 @@ from faker import Faker
 from psycopg2.extensions import connection as _connection
 from psycopg2.extras import DictCursor, execute_batch
 
-import functional.testdata.tables as ddl_tables
 from functional.settings import test_settings
 from functional.testdata.faker_data import get_faker_data
 from functional.utils.helpers import construct_query
@@ -15,31 +14,9 @@ fake = Faker()
 Faker.seed(0)
 
 
-@pytest.fixture(scope='session')
-def create_tables(pg_conn: _connection,
-                  pg_cursor: DictCursor,
-                  ) -> Generator[None, None, None]:
-    """Создать таблицы БД."""
-    pg_cursor.execute(ddl_tables.drop_roles_users)
-    pg_cursor.execute(ddl_tables.drop_roles)
-    pg_cursor.execute(ddl_tables.drop_user)
-    pg_cursor.execute(ddl_tables.drop_login_histories)
-    pg_cursor.execute(ddl_tables.create_user)
-    pg_cursor.execute(ddl_tables.create_login_history)
-    pg_cursor.execute(ddl_tables.create_roles)
-    pg_cursor.execute(ddl_tables.create_roles_users)
-    pg_conn.commit()
-    yield
-    pg_cursor.execute(ddl_tables.drop_roles_users)
-    pg_cursor.execute(ddl_tables.drop_login_histories)
-    pg_cursor.execute(ddl_tables.drop_roles)
-    pg_cursor.execute(ddl_tables.drop_user)
-
-
 @pytest.fixture(scope='function')
 def db_insert_fake_data(pg_conn: _connection,
                         pg_cursor: DictCursor,
-                        create_tables: None,
                         ) -> Generator[None, None, None]:
     """Заполнить таблицы фейковыми данными."""
     faker_data = get_faker_data()
