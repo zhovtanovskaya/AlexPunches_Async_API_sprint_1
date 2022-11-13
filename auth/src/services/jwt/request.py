e4b0363441c00a34c6730068f5b9218730565cf4
Y.Zhovtanovskaya <y.zhovtanovskaya@gmail.com>
2022-11-13 14:22:05 +0600

    Добавлен декоратор @admin_required #51

--- auth/src/services/jwt/request.py
+++ auth/src/services/jwt/request.py
@@ -1,5 +1,27 @@
 """Утилиты для работы с JWT из запроса."""
+from functools import wraps
+from http import HTTPStatus
 
-from flask_jwt_extended import get_jwt, jwt_required
+from flask import jsonify
+from flask_jwt_extended import get_jwt, jwt_required, verify_jwt_in_request
 
-__all__ = ['get_jwt', 'jwt_required']
+from core.config import config
+
+__all__ = ['get_jwt', 'jwt_required', 'admin_required']
+
+
+def admin_required():
+    """Убедиться, что пользователь залогинен и это админ."""
+    def wrapper(fn):
+        @wraps(fn)
+        def decorator(*args, **kwargs):
+            verify_jwt_in_request()
+            claims = get_jwt()
+            if config.admin_role_name in claims.get('roles', ()):
+                return fn(*args, **kwargs)
+            else:
+                msg = 'Нет прав администратора.'
+                return jsonify(msg=msg), HTTPStatus.FORBIDDEN
+        return decorator
+
+    return wrapper
