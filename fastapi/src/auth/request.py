3e86469166f174a0b69ab0dd0a370e6d7c4d733c
Y.Zhovtanovskaya <y.zhovtanovskaya@gmail.com>
2022-11-20 23:22:10 +0600

    Добавлена валидация формата JWT в JWTBearer в fastapi/ #66

--- fastapi/src/auth/request.py
+++ fastapi/src/auth/request.py
@@ -5,15 +5,27 @@ import jwt
 from fastapi import HTTPException, Request, Security
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from jwt import DecodeError, InvalidSignatureError
+from pydantic import BaseModel, Field, ValidationError
 
 from auth.exceptions import AuthorizationException
 from core.config import config
 
 
 class AuthErrors:
+    """Тексты ошибок авторизации."""
+
     NO_TOKEN: str = 'Нет токена авторизации.'
     INVALID_SCHEME: str = 'Схема авторизации не "Bearer".'
     INVALID_TOKEN: str = 'Токен авторизации не валиден.'
+    INVALID_FORMAT: str = (
+        'Не знакомый формат полезной нагрузки токена авторизации.'
+    )
+
+
+class AccessTokenPayload(BaseModel):
+    """Ожидаемый формат данных в токене авторизации JWT."""
+    roles: list[str]
+    type: str = Field('access', const=True)
 
 
 class JWTBearer(HTTPBearer):
@@ -52,19 +64,23 @@ class JWTBearer(HTTPBearer):
         if not credentials.scheme == 'Bearer':
             raise AuthorizationException(AuthErrors.INVALID_SCHEME)
         try:
-            self.verify_jwt(token=credentials.credentials)
+            token = self.decode_jwt(token=credentials.credentials)
         except (DecodeError, InvalidSignatureError) as e:
             raise AuthorizationException(AuthErrors.INVALID_TOKEN) from e
+        except ValidationError as e:
+            raise AuthorizationException(AuthErrors.INVALID_FORMAT) from e
         return credentials
 
-    def verify_jwt(self, token: str):
+    def decode_jwt(self, token: str) -> AccessTokenPayload:
+        """Декодировать и провалидировать полезную нагрузку JWT."""
         payload = jwt.decode(token, self.secret, algorithms=[self.algorithm])
-        print(token)
-        print(payload)
+        return AccessTokenPayload(**payload)
 
 
 jwt_bearer = JWTBearer()
 
 
-async def verify_jwt_token(token: HTTPAuthorizationCredentials = Security(jwt_bearer)):
+async def verify_jwt_token(
+        token: HTTPAuthorizationCredentials = Security(jwt_bearer),
+):
     print(token)
