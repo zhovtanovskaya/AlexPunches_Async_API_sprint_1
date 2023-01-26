cfe9c82fc94a66c21bf55627c0a66a8b495d23f5
Y.Zhovtanovskaya <y.zhovtanovskaya@gmail.com>
2023-01-26 16:50:24 +0500

    Добавлены новые агрегирующие запросы в черновик 2023-01-25_one_collection_queries.js
    * Добавлено создание новых тестовых объектов в 2023-01-25_one_collection_docs.js.
    * Добавлены запросы в 2023-01-25_one_collection_queries.js, которые
      пригодятся для 2023-01-26_rest_api_draft_txt.

--- documentation/design/sprint_9/mongo_schema/2023-01-25_one_collection_docs.js
+++ documentation/design/sprint_9/mongo_schema/2023-01-25_one_collection_docs.js
@@ -1,8 +1,6 @@
 // Примеры документов реакций пользователей на контент
 // для одной коллекции "reactions".
 // Версия от 2023-01-25.
-
-// Рейтинг фильма.
 db.reactions.insertOne({
     // _id
     created_at: new Date(),
@@ -11,6 +9,17 @@ db.reactions.insertOne({
     target_type: "movie",
     type: "rating",
     value: 5    // [0..10]
+    // value: 'neutral',
+})
+db.reactions.insertOne({
+    // _id
+    created_at: new Date(),
+    user_id: "60bf29d4-9c6f-11ed-a1a5-7831c1bc31e4",          // UUID.
+    target_id: "6fdcc8ca-9c6f-11ed-9800-7831c1bc31e4",        // Movie UUID.
+    target_type: "movie",
+    type: "rating",
+    value: 8    // [0..10]
+    // value: 'neutral',
 })
 // Обзор на фильм.
 db.reactions.insertOne({
@@ -19,8 +28,20 @@ db.reactions.insertOne({
     target_id: "6fdcc8ca-9c6f-11ed-9682-7831c1bc31e4",        // Movie UUID.
     target_type: "movie",
     type: "review",
-    value: 5, // [0, 5, 10]
+    value: 5, // [0 - отрицательная, 5 - нейтральная, 10 - положительная]
+    // value: 'thumb_up'
     title: "So-so movie.",
+    text: "Long and detailed explanations..."
+})
+db.reactions.insertOne({
+    created_at: new Date(),
+    user_id: "60bf29d4-9c6f-11ed-9400-7831c1bc31e4",          // UUID.
+    target_id: "6fdcc8ca-9c6f-11ed-9682-7831c1bc31e4",        // Movie UUID.
+    target_type: "movie",
+    type: "review",
+    value: 10, // [0 - отрицательная, 5 - нейтральная, 10 - положительная]
+    // value: 'thumb_up'
+    title: "Must see!",
     text: "Long and detailed explanations..."
 })
 // Лайк на обзор.
@@ -28,6 +49,15 @@ db.reactions.insertOne({
     // _id
     created_at: new Date(),
     user_id: "af18023d-9c76-11ed-9485-7831c1bc31e4",          // UUID.
+    target_id: ObjectId("63d0c92bf5eb85d9a10bd8ac"),          // Review UUID.
+    target_type: "review",
+    type: "like",
+    value: 10, // [0, 10]
+})
+db.reactions.insertOne({
+    // _id
+    created_at: new Date(),
+    user_id: "af18023d-9c76-11ed-9400-7831c1bc31e4",          // UUID.
     target_id: ObjectId("63d0c92bf5eb85d9a10bd8ac"),          // Review UUID.
     target_type: "review",
     type: "like",
--- documentation/design/sprint_9/mongo_schema/2023-01-25_one_collection_queries.js
+++ documentation/design/sprint_9/mongo_schema/2023-01-25_one_collection_queries.js
@@ -1,7 +1,10 @@
 // Запросы к схеме документов one_collection_docs.js
 // Версия от 2023-01-25.
 
-// Посчитать средний рейтинг фильма.
+// Переключиться на тестовую базу данных.
+use ugc
+
+// Посчитать среднюю оценку фильма и число оценок у фильмов.
 db.reactions.aggregate([
     { $match: {"target_type": "movie", "type": "rating"} },
     {
@@ -11,23 +14,37 @@ db.reactions.aggregate([
     },
     { $sort: { avgRatings: -1 } }
 ])
+// Посчитать количество рецензий на фильм.
+db.reactions.aggregate([
+    {
+        $match: {
+            "target_type": "movie",
+            "type": "review",
+            "target_id": "6fdcc8ca-9c6f-11ed-9682-7831c1bc31e4",
+        }
+    },
+    {
+        $group: {
+            _id: "$value", count: { $count : { } }
+        }
+    },
+])
+// Получить реакции конкретного пользователя.
+db.reactions.aggregate([
+    {
+        $match: {
+            $or: [{type: "rating"}, {type: "review"},],
+            "user_id": "60bf29d4-9c6f-11ed-9400-7831c1bc31e4",
+            "target_id": "6fdcc8ca-9c6f-11ed-9682-7831c1bc31e4",
+        }
+    },
+])
 // Посчитать количество лайков у ревью на фильм.
 db.reactions.aggregate([
     { $match: {"target_type": "review", "type": "like"} },
     {
         $group: {
             _id: "$target_id", totalLikes: { $count : { } }
-        }
-    },
-    { $sort: { totalLikes: -1 } }
-])
-db.reactions.aggregate([
-    { $match: {
-        $or: {"target_type": ["review", "like", "rating"] }
-    },
-    {
-        $group: {
-            _id: "$target_id", totalLikes: { $avg : "$value" }
         }
     },
     { $sort: { totalLikes: -1 } }
@@ -50,17 +67,59 @@ db.reactions.find(
 db.reactions.find({
     _id: ObjectId("63d0c92bf5eb85d9a10bd8ac"),
 })
-// Вот этот запрос не возвращает ничего. Хотя выше
-// запрос возвращает объект по этому _id.
+// Вот этот запрос выбрасывает ошибку.  Потому что
+// у UUID формат не подходит для ObjectId.
 db.reactions.find({
   $or: [
       {_id: ObjectId("63d0c92bf5eb85d9a10bd8ac")},
       {_id: ObjectId("6fdcc8ca-9c6f-11ed-9682-7831c1bc31e4")},
   ],
 })
+// Выбрать ревью или лайки.
 db.reactions.find({
   $or: [
       {type: "review"},
       {type: "like"},
   ],
 })
+// Получить положительные рецензии на конкретный фильм.
+db.reactions.find(
+    {
+        target_type: "movie",
+        type: "review",
+        target_id: "6fdcc8ca-9c6f-11ed-9682-7831c1bc31e4",
+        value: 10,      // 10 -- положительная рецензия.
+    }
+)
+// Получить родительские объекты вверх до корня у
+// каждой реакции.
+db.reactions.aggregate([
+    {
+        $graphLookup: {
+            from: "reactions",
+            startWith: "$target_id",
+            connectFromField: "target_id",
+            connectToField: "_id",
+            as: "reactionsHierarchy"
+        }
+   }
+])
+// Посчитать средний рейтинг фильма на основе двух типов
+// реакции: оценок и обзоров.  Упорядочить результат от
+// большего среднего значения к меньшему.
+db.reactions.aggregate([
+    {
+        $match: {
+            "target_type": "movie",
+            $or: [{type: "rating"}, {type: "review"},],
+        }
+    },
+    {
+        $group: {
+            _id: "$target_id",
+            avgReaction: { $avg : "$value" },
+            totalReactions: { $count : { } }
+        }
+    },
+    { $sort: { avgReactions: -1 } }
+])
