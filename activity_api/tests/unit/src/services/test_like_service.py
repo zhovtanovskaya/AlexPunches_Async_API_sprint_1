a27b2ac14c0b9bead27608892c33d8677b997e7b
Y.Zhovtanovskaya <y.zhovtanovskaya@gmail.com>
2023-02-03 19:19:01 +0500

    Добавлены настройки для юнит-тестов в activity_api #115

--- activity_api/tests/unit/src/services/test_like_service.py
+++ activity_api/tests/unit/src/services/test_like_service.py
@@ -3,17 +3,17 @@ import unittest
 from motor.motor_asyncio import AsyncIOMotorClient
 
 from src.services.like import LikeService
+from tests.unit.src.core.config import settings
 
 
 class TestLikeServiceCreate(unittest.IsolatedAsyncioTestCase):
 
     def setUp(self):
-        mongo_url = "mongodb+srv://yana:vAhgeeoxP8crYvpE@cluster0.ynuujij.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-        client = AsyncIOMotorClient(mongo_url)
+        client = AsyncIOMotorClient(settings.test_mongo_url)
         self.service = LikeService(client.test_ugc)
 
     async def test(self):
-        await self.service.delete('63dd0af2e08ddbc859936347')
+        await self.service.delete('63dd1670e08ddbc85993634c')
 
 
 if __name__ == '__main__':
