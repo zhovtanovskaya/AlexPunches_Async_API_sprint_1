9ebe5b9a04eb1222530de6a97d4bd071c5e1acf1
Y.Zhovtanovskaya <y.zhovtanovskaya@gmail.com>
2023-02-04 12:46:40 +0500

    Переименован метод LikeService.find_one(id) #115
    В LikeService.get(id).  Добавлены юнит-тесты для
    метода .delete(id).

--- activity_api/src/services/like.py
+++ activity_api/src/services/like.py
@@ -10,16 +10,16 @@ class LikeService:
         self.mongo = mongo
         self.collection = self.mongo.reactions
 
-    async def find_one(self, id: str):
+    async def get(self, id: str):
         doc = await self.collection.find_one({'_id': ObjectId(id)})
-        return Like(**doc)
+        return Like(**doc) if doc else None
 
     async def create(self, obj: Like) -> Like:
         obj_dict = obj.dict()
         del obj_dict['id']
         result = await self.collection.insert_one(obj_dict)
         if result.acknowledged:
-            return await self.find_one(result.inserted_id)
+            return await self.get(result.inserted_id)
 
     async def delete(self, id: str):
         result = await self.collection.delete_one({'_id': ObjectId(id)})
--- activity_api/tests/unit/src/services/test_like_service.py
+++ activity_api/tests/unit/src/services/test_like_service.py
@@ -7,20 +7,25 @@ from src.services.models.user_content import Like, LikeValue
 from tests.unit.src.core.config import settings
 
 
-class TestLikeServiceCreate(unittest.IsolatedAsyncioTestCase):
+class TestLikeService(unittest.IsolatedAsyncioTestCase):
 
     def setUp(self):
         client = AsyncIOMotorClient(settings.test_mongo_url)
         self.service = LikeService(client.test_ugc)
-
-    async def test_create(self):
-        like = Like(
+        self.like = Like(
             user_id='af18023d-9c76-11ed-9485-7831c1bc31e4',
             target_id='63d0c92bf5eb85d9a10bd8ac',
             value=LikeValue.LIKE,
         )
-        new_like = await self.service.create(like)
-        self.assertIsNotNone(new_like.id)
+
+    async def test_create(self):
+        new_like = await self.service.create(self.like)
+        self.assertIsNotNone(await self.service.get(new_like.id))
+
+    async def test_delete(self):
+        new_like = await self.service.create(self.like)
+        await self.service.delete(new_like.id)
+        self.assertIsNone(await self.service.get(new_like.id))
 
 
 if __name__ == '__main__':
