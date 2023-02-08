44a29713a09a8994513114e1318b8d67b95c584f
Y.Zhovtanovskaya <y.zhovtanovskaya@gmail.com>
2023-02-08 10:30:25 +0500

    Удален пакет tests/unit/src/db/ #115

--- activity_api/tests/unit/src/db/mongo.py
+++
@@ -1,5 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-
-mongo_url = "mongodb+srv://yana:vAhgeeoxP8crYvpE@cluster0.ynuujij.mongodb.net/test_ugc?retryWrites=true&w=majority"
-client = AsyncIOMotorClient(mongo_url)
-mongo = client.test_ugc
