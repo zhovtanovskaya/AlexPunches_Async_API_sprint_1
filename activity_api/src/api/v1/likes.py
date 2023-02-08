bb2c95ae6fd67ead366d120681e57677fc5e078e
Y.Zhovtanovskaya <y.zhovtanovskaya@gmail.com>
2023-02-08 22:28:46 +0500

    Удалена функция get_like_service() в activity_api #121

--- activity_api/src/api/v1/likes.py
+++ activity_api/src/api/v1/likes.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter, Depends
 
 from db.mongo import get_mongo_db
-from services.like import get_like_service
 
 router = APIRouter()
 
@@ -10,7 +9,6 @@ router = APIRouter()
 async def delete_like(
         like_id: str,
         db = Depends(get_mongo_db),
-        service = Depends(get_like_service),
         ) -> None:
     async for doc in db.reactions.find({'type': 'like'}):
         print(doc)
--- activity_api/src/services/like.py
+++ activity_api/src/services/like.py
@@ -1,10 +1,3 @@
-from functools import lru_cache
-
-from fastapi import Depends
-from motor.motor_asyncio import AsyncIOMotorDatabase
-
-from db.mongo import get_mongo_db
-
 from .base import ReactionService
 from .models.user_content.likes import Like
 
@@ -12,10 +5,3 @@ from .models.user_content.likes import Like
 class LikeService(ReactionService):
 
     user_content_type = Like
-
-
-@lru_cache()
-async def get_like_service(
-        mongo_db: AsyncIOMotorDatabase = Depends(get_mongo_db),
-        ) -> LikeService:
-    return LikeService(mongo_db)
