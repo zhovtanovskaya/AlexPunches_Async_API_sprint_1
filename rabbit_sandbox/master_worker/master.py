d92f3d1ae6923404fc223c670a0cc60045e677a9
Punches <punche@yandex.ru>
2023-02-17 22:25:21 +0300

    посмотреть что такое mastre-worker

---
+++ rabbit_sandbox/master_worker/master.py
@@ -0,0 +1,31 @@
+import asyncio
+
+from aio_pika import connect_robust
+from aio_pika.patterns import Master
+
+
+async def main() -> None:
+    connection = await connect_robust(
+        "amqp://user:123456@158.160.34.217:5672/",
+    )
+
+    async with connection:
+        # Creating channel
+        channel = await connection.channel()
+
+        master = Master(channel)
+
+        # Creates tasks by proxy object
+        for task_id in range(10):
+            await master.proxy.my_task_name(task_id=task_id)
+
+        # Or using create_task method
+        for task_id in range(10):
+            await master.create_task(
+                "my_task_name", kwargs=dict(task_id=task_id),
+            )
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
