19609688e47e5ebb8aa15b012b0af68ac966e298
Punches <punche@yandex.ru>
2023-02-18 17:23:18 +0300

    добавить комментарий

--- worker_notifications/src/core/config.py
+++ worker_notifications/src/core/config.py
@@ -15,14 +15,19 @@ class AmqpConect(BaseSettings):
     queue_host: str = '158.160.55.33'
     queue_port: str = '5672'
 
-    def get_conn(self):
-        return f'amqp://{self.queue_user}:{self.queue_password}@{self.queue_host}:{self.queue_port}/'
+    def get_conn(self) -> str:
+        """Получить троку подключения к Реббиту."""
+        return str(
+            f'amqp://{self.queue_user}:{self.queue_password}@'
+            f'{self.queue_host}:{self.queue_port}/'
+        )
 
 
 class WorkerSettings(BaseSettings):
     project_name: str = Field('Movies', env='project_name')
     queue_name: str = 'notifications'
     queue_conn: str = AmqpConect().get_conn()
+    def_priority: int = 5
 
 
 @lru_cache()
--- worker_notifications/src/main.py
+++ worker_notifications/src/main.py
@@ -1,7 +1,9 @@
 import asyncio
+from asyncio import sleep
 
 from aio_pika import connect
 from aio_pika.abc import AbstractIncomingMessage
+from aio_pika.patterns import NackMessage
 
 from core.config import config, logger
 from utils import messages as msg
@@ -9,6 +11,13 @@ from utils import messages as msg
 
 async def on_message(message: AbstractIncomingMessage) -> None:
     async with message.process():
+        # проваледировать возможность отправки (часовой пояс и т.п)
+        # определить в какой отправлятель передать
+
+        # try
+        #    # отправка
+        # except
+        #    # Как вернуть в начало очереди (или в другую очередь?) или ETL-ю
         print(f'\nMessage: {message.body!r}')
         logger.info(msg.message_sent + '\n')
 
@@ -23,6 +32,11 @@ async def main() -> None:
         queue = await channel.declare_queue(
             config.queue_name,
             durable=True,
+            exclusive=False,
+            auto_delete=False,
+            arguments={
+                'x-max-priority': 10,
+            }
         )
 
         await queue.consume(on_message)
