1b70674cc23ef2c3ec19b05aee1f5216bb817118
Y.Zhovtanovskaya <y.zhovtanovskaya@gmail.com>
2023-02-21 15:36:34 +0500

    Добавлены настройки подключения к Kafka в etl_notifications/

--- etl_notifications/src/core/config.py
+++ etl_notifications/src/core/config.py
@@ -1,7 +1,21 @@
+from ipaddress import IPv4Address
+
 from pydantic import BaseSettings
 
 
 class KafkaSettings(BaseSettings):
-    topic = 'my_topic'
-    bootstrap_servers = 'localhost:9092'
-    group_id = "my-group"
+    notifications_topic: str
+    event_store_host: IPv4Address | str
+    event_store_port: int = 9092
+    notifications_group_id: str
+
+    @property
+    def bootstrap_servers(self):
+        return f'{self.event_store_host}:{self.event_store_port}'
+
+
+class Settings(BaseSettings):
+    kafka = KafkaSettings()
+
+
+settings = Settings()
--- etl_notifications/src/main.py
+++ etl_notifications/src/main.py
@@ -1,23 +1,28 @@
 import asyncio
 
-from extract.extractor import Extractor
-from load.loader import Loader
-from transform.transformer import Transformer
+from aiokafka import AIOKafkaConsumer
+
+from core.config import settings
+from db import kafka
+from etl import etl
 
 
-async def etl():
-    extractor = Extractor()
-    transformer = Transformer()
-    loader = Loader()
-    async for event in extractor.extract():
-        print(event)
-        await transformer.add_event(event)
-        posting = await transformer.get_posting()
-        await loader.load(posting)
+async def startup():
+    kafka.consumer = AIOKafkaConsumer(
+        settings.kafka.notifications_topic,
+        bootstrap_servers=settings.kafka.bootstrap_servers,
+        group_id=settings.kafka.notifications_group_id,
+    )
+    await kafka.consumer.start()
+
+
+async def main():
+    await startup()
+    await etl()
 
 
 if __name__ == '__main__':
     try:
-        asyncio.run(etl())
+        asyncio.run(main())
     except KeyboardInterrupt:
         pass
