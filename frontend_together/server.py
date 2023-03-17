43c936300f2bd8c11357b2c1237f1fc69a820512
Punches <punche@yandex.ru>
2023-03-17 23:31:24 +0300

    сделать фронт повеселее

--- frontend_together/server.py
+++ frontend_together/server.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__,
-            static_url_path='/static/',
-            static_folder='static',
+            static_url_path='/assets/',
+            static_folder='assets',
             template_folder='templates',
             )
 
